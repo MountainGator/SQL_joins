e559ee8bbcc2e819a510ff9701645fe123a3650d
LukeSandersJet <lsanders@jet-airways.com>
2022-06-14 12:03:44 -0500

    solve exercises

--- queries.sql
+++ queries.sql
@@ -1,3 +1,14 @@
 -- write your queries here
 SELECT * FROM owners o FULL JOIN vehicles v ON o.id = v.owner_id;
 
+SELECT first_name, last_name, COUNT(owner_id) from owners o
+    JOIN vehicles v ON o.id = v.owner_id
+    GROUP BY (first_name, last_name)
+    ORDER BY first_name;
+
+SELECT first_name, last_name, COUNT(owner_id) AS total_cars, ROUND(AVG(price)) AS average_price 
+	FROM owners o
+    JOIN vehicles v ON o.id = v.owner_id
+    GROUP BY (first_name, last_name)
+    HAVING COUNT(owner_id) > 1 AND AVG(price) > 10000
+    ORDER BY COUNT(owner_id) desc;
